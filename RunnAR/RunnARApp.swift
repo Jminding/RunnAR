762699d16d2be1b1e7c701527be88efec81af14e
Jaymin Ding <jtding43@gmail.com>
2022-08-08 12:56:39 -0400

    Initial Commit

---
+++ RunnAR/RunnARApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnARApp.swift
+//  RunnAR
+//
+//  Created by Jaymin Ding on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RunnARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
